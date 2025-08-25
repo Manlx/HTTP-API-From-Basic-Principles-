ecab1e2921d56710b36ec209749915537bb380d4
Manuel Nunes <marnununes@gmail.com>
2025-08-26 01:49:59 +0200

    Add SQL to create database in accordance with design;

--- database.js
+++ database.js
@@ -21,21 +21,157 @@ export function ResetDB(dbCon) {
 
   dbCon.open();
 
-  dbCon.exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)");
+  dbCon.exec(`
+    CREATE TABLE IF NOT EXISTS Users (
+      Id INTEGER PRIMARY KEY, 
+      UserName TEXT UNIQUE, 
+      DisplayName TEXT, 
+      Password TEXT, 
+      IsDeleted INTEGER
+    )
+  `);
 
-  dbCon.exec("CREATE TABLE IF NOT EXISTS roles (id INTEGER PRIMARY KEY, name TEXT, description TEXT)");
-  
-  const userInsert = dbCon.prepare("INSERT INTO users(name, email) values (?, ?)");
+  dbCon.exec(`
+    CREATE TABLE IF NOT EXISTS ToDoCategories (
+      Id INTEGER PRIMARY KEY, 
+      Description TEXT
+    )
+  `);
 
-  userInsert.run('Person A','PersonA@gmail.com')
+  dbCon.exec(`
+    CREATE TABLE IF NOT EXISTS ToDoItemUserStatus (
+      Id INTEGER PRIMARY KEY, 
+      Status TEXT
+    )
+  `);
 
-  userInsert.run('Person B','PersonB@gmail.com')
+  dbCon.exec(`
+    CREATE TABLE IF NOT EXISTS ToDoItems (
+      Id INTEGER PRIMARY KEY, 
+      DisplayName TEXT, 
+      ToDoType INTEGER, 
+      FOREIGN KEY(ToDoType) REFERENCES ToDoCategories(Id)
+    )
+  `);
 
-  userInsert.run('Person C','PersonC@gmail.com')
+  dbCon.exec(`
+    CREATE TABLE IF NOT EXISTS UserToDoItems (
+      Id INTEGER PRIMARY KEY, 
+      UserId INTEGER, 
+      ToDoId INTEGER, 
+      IsDeleted INTEGER, 
+      CreatedOn INTEGER, 
+      CompletedOn INTEGER, 
+      CurrentStatus INTEGER, 
+      FOREIGN KEY (UserId) REFERENCES Users(Id),
+      FOREIGN KEY (ToDoId) REFERENCES ToDoItems(Id),
+      FOREIGN KEY (CurrentStatus) REFERENCES ToDoItemUserStatus(Id)
+    )
+  `);
 
-  userInsert.run('Person C','PersonC@gmail.com')
+  const toDoItemUserStatusInsert = dbCon.prepare(`
+    INSERT INTO ToDoItemUserStatus (
+      Status
+    ) VALUES (
+      ?
+    )
+  `);
+
+  toDoItemUserStatusInsert.run('Doing')
+  toDoItemUserStatusInsert.run('Done')
+  toDoItemUserStatusInsert.run('Back Log')
+  toDoItemUserStatusInsert.run('Pending')
+
+  const usersInsert = dbCon.prepare(`
+    INSERT INTO Users (
+      UserName,
+      DisplayName,
+      Password,
+      IsDeleted
+    ) VALUES (
+      ?,
+      ?,
+      ?,
+      ?
+    )
+  `)
+
+  usersInsert.run('JacoPretorius', 'xXJacoXx', 'myPassword1234',0)
+  usersInsert.run('DanielDavidson', 'Dany_D', 'DannyBigD',0)
+  usersInsert.run('Jeckob', 'J-Man', 'Jeckob1997',0)
+
+  const toDoCategoriesInsert = dbCon.prepare(`
+    INSERT INTO ToDoCategories (
+      Description
+    ) VALUES (
+     ?
+    )
+  `)
+
+  toDoCategoriesInsert.run('Shopping')
+  toDoCategoriesInsert.run('Cleaning')
+  toDoCategoriesInsert.run('Maintenance')
+  toDoCategoriesInsert.run('Admin')
+
+  dbCon.exec(`
+    INSERT INTO ToDoItems (
+      DisplayName,
+      ToDoType
+    ) VALUES (
+      'Buy Shoes',
+      (
+        SELECT Id 
+        FROM ToDoCategories 
+        WHERE Description = 'Shopping'
+      )
+    )
+  `)
+
+  dbCon.exec(`
+    INSERT INTO ToDoItems (
+      DisplayName,
+      ToDoType
+    ) VALUES (
+      'Kitchen',
+      (
+        SELECT Id 
+        FROM ToDoCategories 
+        WHERE Description = 'Cleaning'
+      )
+    )
+  `)
+
+  dbCon.exec(`
+    INSERT INTO ToDoItems (
+      DisplayName,
+      ToDoType
+    ) VALUES (
+      'Gutters',
+      (
+        SELECT Id 
+        FROM ToDoCategories 
+        WHERE Description = 'Maintenance'
+      )
+    )
+  `)
+
+  // const userInsert = dbCon.prepare("INSERT INTO users(name, email) values (?, ?)");
+
+  // userInsert.run('Person A','PersonA@gmail.com')
+
+  // userInsert.run('Person B','PersonB@gmail.com')
+
+  // userInsert.run('Person C','PersonC@gmail.com')
+
+  // userInsert.run('Person C','PersonC@gmail.com')
 
   // const selectAllUsers = dbCon.prepare(`SELECT * FROM users;`)
+
+  // console.log(dbCon.prepare(`SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name;`).all())
+  // console.log(dbCon.prepare(`SELECT * FROM ToDoItemUserStatus`).all())
+  // console.log(dbCon.prepare(`SELECT * FROM Users`).all())
+  // console.log(dbCon.prepare(`SELECT * FROM ToDoCategories`).all())
+  console.log(dbCon.prepare(`SELECT * FROM ToDoItems`).all())
   
   dbCon.close()
 }
