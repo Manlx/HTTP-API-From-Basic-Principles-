697581ed45b83b8d1b5c978f5e2c3a7b2c4b6973
Manuel Nunes <marnununes@gmail.com>
2025-08-27 15:11:09 +0200

    Add type for token;

--- types.js
+++ types.js
@@ -2,6 +2,13 @@
  * @typedef {| "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"} HTTPMethods
  */
 
+/**
+ * @typedef {object} AuthToken
+ * @prop {number} userId
+ * @prop {number} issuedAt
+ * @prop {number} expiresAt
+ */
+
 
 /**
  * @namespace DataStructures
