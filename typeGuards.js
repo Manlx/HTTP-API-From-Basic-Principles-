39b3433571df94d1b7a4f401358167fae0e6f2a5
Manuel Nunes <marnununes@gmail.com>
2025-08-30 04:36:22 +0200

    Add functions to create proof functions;

---
+++ typeGuards.js
@@ -0,0 +1,57 @@
+/** @import {
+ * CreateType,
+ * LoginBody,
+ * typeOfReturn
+ * } from "./types.js" */
+
+
+/**
+ * Proves variable contains object type
+ * @param {unknown} possibleObject 
+ * @returns {possibleObject is object}
+ */
+export function isObject(possibleObject) {
+  
+  return typeof possibleObject === "object" && !Array.isArray(possibleObject)
+}
+
+/**
+ * Generates a function to prove that some object based type is a specific sub object type
+ * @template { Object<string,typeOfReturn> | Object<number,typeOfReturn> } ProvenType
+ * @param {CreateType<ProvenType>} exampleObject
+ * @returns {(data: unknown) => data is ProvenType}
+ */
+export function GenerateObjectTypeProof(exampleObject){
+
+  const keys = Object.keys(exampleObject)
+  
+  return /** @type { (data: unknown) => data is ProvenType } */ ((data) => {
+
+    if (!isObject(data)) {
+
+      return false;
+    }
+
+    for (let i = 0; i < keys.length; i++) {
+
+      const currentKey = keys[i];
+      const keyValueType = exampleObject[currentKey];
+
+      if (
+        !(currentKey in data) ||
+        typeof data[data] !== keyValueType
+      ){
+
+        return false;
+      }
+    }
+
+    return true;
+  })
+
+}
+
+export const isLoginBody = /** @type {typeof GenerateObjectTypeProof<LoginBody>} */(GenerateObjectTypeProof)({
+  password: 'string',
+  username: 'string'
+})
