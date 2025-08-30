69e78f29def7f68b07499f1b7fb625886c6a675c
Manuel Nunes <marnununes@gmail.com>
2025-08-30 04:30:24 +0200

    Add types and utility types;

--- types.js
+++ types.js
@@ -1,3 +1,4 @@
+/** @import http from "http" */
 /**
  * @typedef {| "GET" | "HEAD" | "POST" | "PUT" | "DELETE" | "CONNECT" | "OPTIONS" | "TRACE" | "PATCH"} HTTPMethods
  */
@@ -18,6 +19,55 @@
 /**
  * @memberof DataStructures
  * @typedef {"Yes"|"No"} Answers
+ */
+
+/**
+ * @typedef {object} RouteHandler
+ * @prop {string} route
+ * @prop {(req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>, route: string ) => void} handler
+ */
+
+/** @typedef {(req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage>, route: string ) => Promise<void>} HandlerFunction */
+
+/**
+ * @typedef {object} LoginBody
+ * @prop {string} username
+ * @prop {string} password
+ */
+
+/**
+ * @typedef { "undefined" | "object" | "boolean" | "number" | "bigint" | "string" | "symbol" | "unknown"} typeOfReturn
+ */
+
+/**
+ * Derives the string representation of a type similar to the `typeof` operator.
+ *
+ * @template T The type to analyze.
+ * @typedef {T extends string
+ *   ? Extract<typeOfReturn, 'string'>
+ *   : T extends object
+ *   ? Extract<typeOfReturn, 'object'>
+ *   : T extends number
+ *   ? Extract<typeOfReturn, 'number'>
+ *   : T extends boolean
+ *   ? Extract<typeOfReturn, 'boolean'>
+ *   : T extends bigint
+ *   ? Extract<typeOfReturn, 'bigint'>
+ *   : T extends symbol
+ *   ? Extract<typeOfReturn, 'symbol'>
+ *   : T extends undefined
+ *   ? Extract<typeOfReturn, 'undefined'>
+ *   : "unknown"} stringTypeOf
+ */
+
+/**
+ * Creates a new type where each property's value is the `stringTypeOf`
+ * representation of the original property's type.
+ *
+ * @template T The object type to transform.
+ * @typedef {{
+ *   [key in keyof T]: stringTypeOf<T[key]>
+ * }} CreateType
  */
 
 export default {};
