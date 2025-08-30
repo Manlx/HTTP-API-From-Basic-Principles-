9b45442540a0cfb9df64e0cf02296809eb1890fb
Manuel Nunes <marnununes@gmail.com>
2025-08-30 20:32:06 +0200

    Complete login handler, login handler will verify login information and create a new session token and invalidate previous tokens for that current user;

--- handlers/LoginHandler.js
+++ handlers/LoginHandler.js
@@ -1,7 +1,7 @@
 /** @import http from "http" */
 /** @import {HandlerFunction} from "../types.js" */
 
-import { CanLogin, dbCon, GetUserSessionToken} from "../database.js"
+import { CanLogin, CreateUserSessionToken, dbCon, GetUserSessionToken} from "../database.js"
 import { isLoginBody } from "../typeGuards.js";
 import { GetBodyFromRequest, Handle400, Handle401, Handle404, MakeToken, SetJsonReturn } from "../utils.js"
 
@@ -12,8 +12,6 @@ export const LoginHandler = async (req, res, route) => {
   const body = await GetBodyFromRequest(req,20);
 
   const parsedBody = JSON.parse(body || '{}')
-
-  console.log(parsedBody)
 
   if (!isLoginBody(parsedBody)){
 
@@ -35,27 +33,13 @@ export const LoginHandler = async (req, res, route) => {
   }
 
   const [
-    hasToken,
-    lastSessionToken
-  ] = GetUserSessionToken(dbCon, accountId);
-
-  if (hasToken) {
-
-    // Check if last token is expired
-
-      if (lastSessionToken.IsInvalidated) {
-
-        // If the last session has been invalidated, we can create a new session.
-      }
-
-    return;
-  }
-
+    token,
+  ] = CreateUserSessionToken(dbCon,accountId);
   
   SetJsonReturn(res)
 
   res.end(JSON.stringify({
-    token: MakeToken(accountId)
+    token: token
   }))
 
   return;
