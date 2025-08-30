b4039fbf56eca2b2c05b16549fe8fef98b6d78c8
Manuel Nunes <marnununes@gmail.com>
2025-08-30 06:37:25 +0200

    Continue work on Login handler;

--- handlers/LoginHandler.js
+++ handlers/LoginHandler.js
@@ -1,8 +1,9 @@
 /** @import http from "http" */
 /** @import {HandlerFunction} from "../types.js" */
 
-import { CanLogin, dbCon} from "../database.js"
-import { GetBodyFromRequest, Handle400, SetJsonReturn } from "../utils.js"
+import { CanLogin, dbCon, GetUserSessionToken} from "../database.js"
+import { isLoginBody } from "../typeGuards.js";
+import { GetBodyFromRequest, Handle400, Handle401, Handle404, MakeToken, SetJsonReturn } from "../utils.js"
 
 /** @type {HandlerFunction} */
 
@@ -10,33 +11,51 @@ export const LoginHandler = async (req, res, route) => {
 
   const body = await GetBodyFromRequest(req,20);
 
-  // console.log(JSON.stringify(body))
+  const parsedBody = JSON.parse(body || '{}')
 
-  /** @type {unknown} */
-  const parsedBody = JSON.parse(body || '{}');
+  console.log(parsedBody)
 
-  if (
-    parsedBody === null ||
-    typeof parsedBody !== "object"  || 
-    Array.isArray(parsedBody) ||
-    !("username" in parsedBody) ||
-    !("password" in parsedBody) ||
-    typeof parsedBody.password !== 'string' || 
-    typeof parsedBody.username !== 'string'
-  ) {
+  if (!isLoginBody(parsedBody)){
 
-    Handle400(req, res)
+    Handle400(req,res, 'Invalid body')
 
     return;
   }
 
-  parsedBody
-  
+  const [
+    validLogin,
+    accountId
+  ] = CanLogin(dbCon, parsedBody.UserName, parsedBody.Password);
 
+  if (!validLogin) {
+
+    Handle401(req,res, 'Login failed.')
+    
+    return;
+  }
+
+  const [
+    hasToken,
+    lastSessionToken
+  ] = GetUserSessionToken(dbCon, accountId);
+
+  if (hasToken) {
+
+    // Check if last token is expired
+
+      if (lastSessionToken.IsInvalidated) {
+
+        // If the last session has been invalidated, we can create a new session.
+      }
+
+    return;
+  }
+
+  
   SetJsonReturn(res)
 
   res.end(JSON.stringify({
-    message: "Message: " + CanLogin(dbCon, parsedBody.username, parsedBody.password)
+    token: MakeToken(accountId)
   }))
 
   return;
